27cb4a1f4bfb70b93c7bd0c60b1caa5218c4e0ed
Alejandro Giraldo Duque <alejogo4@users.noreply.github.com>
2021-10-10 22:08:18 -0500

    Initial project

---
+++ rife_rafe_app/android/app/src/main/kotlin/com/example/rife_rafe_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rife_rafe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rife_rafe_backend/rife_rafe_backend/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using rife_rafe_backend.Data;
+
+[assembly: HostingStartup(typeof(rife_rafe_backend.Areas.Identity.IdentityHostingStartup))]
+namespace rife_rafe_backend.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
